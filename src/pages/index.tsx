7527429c00527572f94d74ebb7a53e9458e32608
Vitor Rafael <rqfvitor@gmail.com>
2022-07-15 23:30:42 -0300

    add: about component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { Header } from '../components/Header';
 import { Hero } from '../components/Hero';
+import { About } from '../components/About';
+import { Projects } from '../components/Projects';
 
 export default function Home() {
   return (
     <div className="relative">
       <Header />
       <Hero />
+      <About />
+      <Projects />
     </div>
   );
 }
