a425c5b78789b2d71eb29b0aea61a5e1d9586061
Vitor Rafael <rqfvitor@gmail.com>
2022-07-16 14:22:22 -0300

    fix: import env to process env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: import.meta.env.API_URL,
+  uri: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    Authorization: `Bearer ${import.meta.env.API_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`,
   },
   cache: new InMemoryCache(),
 });
