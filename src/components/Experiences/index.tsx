2f3189136f22eebc49519bc0498f21b659f8b933
Vitor Rafael <rqfvitor@gmail.com>
2022-10-19 18:32:46 -0300

    feat: add experience animation

--- src/components/Experiences/index.tsx
+++ src/components/Experiences/index.tsx
@@ -1,12 +1,18 @@
-import { ExperienceContainer } from './ExperienceContainer/index';
-import { SecTitle } from '../SecTitle/index';
+import Aos from 'aos';
+import { useEffect } from 'react';
+
 import { useGetAllExperiencesQuery } from '../../graphql/generated';
+import { SecTitle } from '../SecTitle';
+import { ExperienceContainer } from './ExperienceContainer';
 
 export function Experiences() {
+  useEffect(() => {
+    Aos.init({ duration: 1200 });
+  }, []);
   const { data } = useGetAllExperiencesQuery();
 
   return (
-    <section className="flex flex-col items-center">
+    <section className="flex flex-col items-center" data-aos="fade-up">
       <SecTitle content="Experiências" />
       <span className="h-0.5 w-full bg-secondary-500"></span>
       <div className="w-full lg:flex-row flex flex-col gap-4 py-8 lg:justify-start lg:overflow-x-scroll lg:scrollbar-thin lg:scrollbar-track-secondary-500 lg:scrollbar-thumb-primary-500">
